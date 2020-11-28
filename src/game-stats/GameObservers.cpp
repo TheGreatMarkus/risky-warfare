45a80eea6d36c6fba305cb3650012574433d3ec5
Cristian Aldea <aldea.cristian@outlook.com>
2020-11-28 17:12:05 -0500

    Fill implementation for observers

--- src/game-stats/GameObservers.cpp
+++ src/game-stats/GameObservers.cpp
@@ -1,59 +1,81 @@
 #include "GameObservers.h"
 
+#include "../utils/Utils.h"
+#include "../GameEngine.h"
+
+
+using cris_utils::removeElement;
 //=============================
 // Observer Implementation
 //=============================
 
-Observer::Observer() {
+Observer::Observer() {}
 
-}
+Observer::Observer(const Observer &other) {}
 
-Observer::Observer(const Observer &other) {
-
-}
-
-void swap(Observer &a, Observer &b) {
-
-}
+void swap(Observer &a, Observer &b) {}
 
 ostream &operator<<(ostream &out, const Observer &obj) {
+    obj.print(out);
     return out;
 }
 
-Observer::~Observer() {
+Observer::~Observer() {}
 
-}
 
 //=============================
 // Subject Implementation
 //=============================
 
-Subject::Subject() {
 
+Subject::Subject() : observers{} {}
+
+Subject::Subject(const Subject &other) : observers{} {
+    for (auto &observer : other.observers) {
+        observers.push_back(observer->clone());
+    }
 }
 
 void swap(Subject &a, Subject &b) {
-
+    using std::swap;
+    swap(a.observers, b.observers);
 }
 
 ostream &operator<<(ostream &out, const Subject &obj) {
+    obj.print(out);
     return out;
 }
 
+void Subject::attach(Observer *observer) {
+    observers.push_back(observer);
+}
+
+void Subject::detach(Observer *observer) {
+    removeElement(observers, observer);
+}
+
+void Subject::notify() {
+    for (auto &observer : observers) {
+        observer->update();
+    }
+}
+
 Subject::~Subject() {
-
+    // Subject is responsible for its observers
+    for (auto &observer : observers) {
+        delete observer;
+    }
 }
 
-//=============================
-// PhaseObserver Implementation
-//=============================
 
-PhaseObserver::PhaseObserver() {
+PhaseObserver::PhaseObserver(Game *game) : game{game} {}
 
-}
+// Since observer isn't responsible for managing the memory of the game, it won't clone it
+PhaseObserver::PhaseObserver(const PhaseObserver &other) : game{other.game} {}
 
 void swap(PhaseObserver &a, PhaseObserver &b) {
-
+    using std::swap;
+    swap(a.game, b.game);
 }
 
 PhaseObserver &PhaseObserver::operator=(PhaseObserver other) {
@@ -61,28 +83,31 @@ PhaseObserver &PhaseObserver::operator=(PhaseObserver other) {
     return *this;
 }
 
-ostream &operator<<(ostream &out, const PhaseObserver &obj) {
-    return out;
-}
-
 void PhaseObserver::update() {
-
+    // TODO implement
 }
 
-PhaseObserver::~PhaseObserver() {
-
+void PhaseObserver::print(ostream &out) const {
+    out << "PhaseObserver{ game:" << game << " }";
 }
 
-//=============================
-// PhaseObserver Implementation
-//=============================
-
-GameStatisticsObserver::GameStatisticsObserver() {
-
+Observer *PhaseObserver::clone() {
+    return new PhaseObserver(*this);
 }
+
+// This observer is not responsible for managing the game's memory, so it won't delete it
+PhaseObserver::~PhaseObserver() {}
+
+
+GameStatisticsObserver::GameStatisticsObserver(Game *game) : game{game} {}
+
+// Since observer isn't responsible for managing the memory of the game, it won't clone it
+GameStatisticsObserver::GameStatisticsObserver(const GameStatisticsObserver &other)
+        : game{other.game} {}
 
 void swap(GameStatisticsObserver &a, GameStatisticsObserver &b) {
-
+    using std::swap;
+    swap(a.game, b.game);
 }
 
 GameStatisticsObserver &GameStatisticsObserver::operator=(GameStatisticsObserver other) {
@@ -90,14 +115,17 @@ GameStatisticsObserver &GameStatisticsObserver::operator=(GameStatisticsObserver
     return *this;
 }
 
-ostream &operator<<(ostream &out, const GameStatisticsObserver &obj) {
-    return out;
-}
-
 void GameStatisticsObserver::update() {
-
+    // TODO implement
 }
 
-GameStatisticsObserver::~GameStatisticsObserver() {
-
+void GameStatisticsObserver::print(ostream &out) const {
+    out << "GameStatisticsObserver{ game:" << game << " }";
 }
+
+Observer *GameStatisticsObserver::clone() {
+    return new GameStatisticsObserver(*this);
+}
+
+// This observer is not responsible for managing the game's memory, so it won't delete it
+GameStatisticsObserver::~GameStatisticsObserver() {}
--- src/game-stats/GameObservers.h
+++ src/game-stats/GameObservers.h
@@ -2,60 +2,82 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::ostream;
 using std::string;
+using std::vector;
+
+class Game;
 
 class Observer {
 public:
     Observer();
     Observer(const Observer &other);
     friend void swap(Observer &a, Observer &b);
-//    Observer &operator=(Observer other); // abstract class, can't overload assignment operator
+    // abstract class, can't overload assignment operator
+    // Observer &operator=(Observer other);
     friend ostream &operator<<(ostream &out, const Observer &obj);
 
     virtual void update() = 0;
-
+    virtual void print(ostream &out) const = 0;
+    virtual Observer *clone() = 0;
 
     virtual ~Observer();
 
 };
 
 class Subject {
+private:
+    vector<Observer *> observers;
+public:
     Subject();
     Subject(const Subject &other);
     friend void swap(Subject &a, Subject &b);
-    //Order &operator=(Order other); // abstract class, can't overload assignment operator
+    // abstract class, can't overload assignment operator
+    //Order &operator=(Order other);
     friend ostream &operator<<(ostream &out, const Subject &obj);
 
-    virtual void notify() = 0;
-    virtual string getState() = 0;
+    void attach(Observer *observer);
+    void detach(Observer *observer);
+    void notify();
+
+    virtual void print(ostream &out) const = 0;
+    virtual Subject *clone() = 0;
+
 
     virtual ~Subject();
 };
 
 class PhaseObserver : public Observer {
-    PhaseObserver();
+private:
+    Game *game;
+public:
+    PhaseObserver(Game *game);
     PhaseObserver(const PhaseObserver &other);
     friend void swap(PhaseObserver &a, PhaseObserver &b);
     PhaseObserver &operator=(PhaseObserver other);
-    friend ostream &operator<<(ostream &out, const PhaseObserver &obj);
 
-    void update();
+    void update() override;
+    void print(ostream &out) const override;
+    Observer *clone() override;
 
 
     virtual ~PhaseObserver();
 };
 
 class GameStatisticsObserver : public Observer {
-    GameStatisticsObserver();
+private:
+    Game *game;
+public:
+    GameStatisticsObserver(Game *game);
     GameStatisticsObserver(const GameStatisticsObserver &other);
     friend void swap(GameStatisticsObserver &a, GameStatisticsObserver &b);
     GameStatisticsObserver &operator=(GameStatisticsObserver other);
-    friend ostream &operator<<(ostream &out, const GameStatisticsObserver &obj);
 
-    void update();
-
+    void update() override;
+    void print(ostream &out) const override;
+    Observer *clone() override;
 
     virtual ~GameStatisticsObserver();
 };
